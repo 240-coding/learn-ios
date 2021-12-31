dd92e51b77e3c7dabdb78b297893c961c6926f11
Seoyoung Lee <wow9176@naver.com>
2022-01-01 00:57:02 +0900

    init project

---
+++ MusicPlayer/MusicPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by 이서영 on 2021/12/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
