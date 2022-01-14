8818503de9069dc75dade8db3a9f40993d4fcec5
Seoyoung Lee <wow9176@naver.com>
2022-01-14 14:44:21 +0900

    [Add] 1, 2번째 화면 gesture recognizer 등록, 다음 버튼 활성화 기능 구현 다음 버튼 활성화 기능은 아직 이미지 선택 조건은 포함이 안 되있어 추후 더 수정해야 됨.

--- SignUpProject/SignUpProject/ViewController.swift
+++ SignUpProject/SignUpProject/ViewController.swift
@@ -8,12 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBAction func tapView(_ sender: UITapGestureRecognizer) {
+        self.view.endEditing(true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
 
 }
 
