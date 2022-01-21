8cd72f324654de11d0154091ceab874f5bdcd6d5
Seoyoung Lee <wow9176@naver.com>
2022-01-21 15:52:21 +0900

    [Add] 셀 재사용, 세그

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,15 +1,36 @@
-import UIKit
 import Foundation
 
-protocol Drivable {}
-protocol Stoppable {}
+var cache = [String: [String]]()
 
-// Drivable & Stoppable : 두 프로토콜 결합
-func create<T: Drivable & Stoppable>(car: T) {
-    
+func combinations(_ array: [String]) -> Set<String> {
+    if array.count == 0 { return [] }
+
+    let answerArray = (0..<array.count).flatMap { i -> [String] in
+        var removedArray = array
+        let elem = removedArray.remove(at: i)
+        return [elem] + combinations(removedArray).map { elem + $0 }
+    }
+//    print("answerArray: \(answerArray)")
+    return Set(answerArray)
 }
-class BMW: Drivable, Stoppable {
-    
+
+func isPrime(_ number: Int) -> Bool {
+    guard number > 1 else {
+        return false
+    }
+
+    for i in 2..<number {
+        if number % i == 0 { return false }
+    }
+    return true
 }
-class Fruit: Codable {
+
+func solution(_ numbers: String) -> Int {
+    let array = Array(numbers).map{ String($0) }
+    print(Set(combinations(array)))
+    let intSet = Set(combinations(array).compactMap { Int($0) })
+    print(intSet)
+    return intSet.filter{ isPrime($0) }.count
 }
+
+print(solution("011"))
--- SimpleTable/SimpleTable/ViewController.swift
+++ SimpleTable/SimpleTable/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
     @IBOutlet weak var tableView: UITableView!
     let cellIdentifier: String = "cell"
+    let customCellIdentifier: String = "customCell"
     
     let korean: [String] = ["가", "나", "다", "라", "마", "바", "사", "아", "자", "차", "카", "타", "파", "하"]
     let english: [String] = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
@@ -18,6 +19,11 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     let dateFormatter: DateFormatter = {
         let formatter: DateFormatter = DateFormatter()
         formatter.dateStyle = .medium
+        
+        return formatter
+    }()
+    let timeFormatter: DateFormatter = {
+        let formatter: DateFormatter = DateFormatter()
         formatter.timeStyle = .medium
         
         return formatter
@@ -54,24 +60,52 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: self.cellIdentifier, for: indexPath)
+        
         
         if indexPath.section < 2 {
+            let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: self.cellIdentifier, for: indexPath)
             let text:String = indexPath.section == 0 ? korean[indexPath.row] : english[indexPath.row]
             cell.textLabel?.text = text
+            
+            if indexPath.row == 1 {
+                cell.backgroundColor = UIColor.red
+            } else {
+                cell.backgroundColor = UIColor.white
+            }
+            
+            return cell
         } else {
-            cell.textLabel?.text = self.dateFormatter.string(from: self.dates[indexPath.row])
+            let cell: CustomTableViewCell = tableView.dequeueReusableCell(withIdentifier: self.customCellIdentifier, for: indexPath) as! CustomTableViewCell
+            cell.leftLabel?.text = self.dateFormatter.string(from: self.dates[indexPath.row])
+            cell.rightLabel?.text = self.timeFormatter.string(from: self.dates[indexPath.row])
+            
+            return cell
         }
        
-        
-        return cell
-    }
+}
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         if section < 2 {
             return section == 0 ? "한글" : "영어"
         }
         return nil
+    }
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+        guard let nextViewController: SecondViewController = segue.destination as? SecondViewController else {
+            return
+        }
+        
+        guard let cell: UITableViewCell = sender as? UITableViewCell else {
+            return
+        }
+        
+        nextViewController.textToSet = cell.textLabel?.text
     }
 }
 
