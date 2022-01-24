90ec8664bddf1ea281a980e41d5dc968e2d094f7
Seoyoung Lee <wow9176@naver.com>
2022-01-25 00:09:06 +0900

    [Add] CoutnryTableViewCell 클래스 생성

---
+++ WeatherToday/WeatherToday/CountryTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  CountryTableViewCell.swift
+//  WeatherToday
+//
+//  Created by 이서영 on 2022/01/25.
+//
+
+import UIKit
+
+class CountryTableViewCell: UITableViewCell {
+    @IBOutlet weak var countryImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
