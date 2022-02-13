7c40097a51c64c6ceb827e66706d863d81368e6a
Seoyoung Lee <wow9176@naver.com>
2022-02-13 23:44:49 +0900

    [Study] Alert, Actionsheet

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,12 +1,4 @@
 import Foundation
 
-func solution(_ left:Int, _ right:Int) -> Int {
-    let a = (left...right).map{ sqrt(Double($0)) == floor(sqrt(Double($0))) ? -$0 : $0 }
-    print(a.reduce(0, +))
-    var b = (left...right).map{ sqrt(Double($0)) == floor(sqrt(Double($0))) ? -$0 : $0 }.reduce(0, +)
-    //    return (left...right).map{ sqrt(Double($0)) == floor(sqrt(Double($0))) ? -$0 : $0 }.reduce(0, +)
-        return 1
-}
-
-solution(13, 17)
-//print(solution(13, 17))
+var a = [Int]()
+"123".reversed()
