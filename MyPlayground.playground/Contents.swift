0661b64b016786192b4aa3f1d72969259096a633
Seoyoung Lee <wow9176@naver.com>
2022-02-26 10:35:12 +0900

    [Study] Adding the sign-in button

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,7 +1,22 @@
 import Foundation
 
-let a = ["cat", "bed", "door"]
+import Foundation
 
-let i = "Zbcdefg"
-print(String(i.reversed()))
-q
+func solution(_ n:Int) -> Int {
+    func isPrime(_ num: Int) -> Bool {
+        if num == 1 { return false }
+        if num == 2 || num == 3 { return true }
+        print(Int(sqrt(Double(num))))
+        for i in 2..<Int(sqrt(Double(num))) {
+            if num % i == 0 {
+                print("\(num) % \(i) OOPS")
+                return false
+            }
+        }
+        return true
+    }
+    print((1...n).filter{ isPrime($0) })
+    return (1...n).filter{ isPrime($0) }.count
+}
+
+print(solution(4))
