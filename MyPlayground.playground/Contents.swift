3e681bc76c7e775c38aaca240f9190a5ad7d394b
Seoyoung Lee <wow9176@naver.com>
2022-03-01 22:49:27 +0900

    [Study] Add tab bar

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,22 +1,3 @@
 import Foundation
 
-import Foundation
 
-func solution(_ n:Int) -> Int {
-    func isPrime(_ num: Int) -> Bool {
-        if num == 1 { return false }
-        if num == 2 || num == 3 { return true }
-        print(Int(sqrt(Double(num))))
-        for i in 2..<Int(sqrt(Double(num))) {
-            if num % i == 0 {
-                print("\(num) % \(i) OOPS")
-                return false
-            }
-        }
-        return true
-    }
-    print((1...n).filter{ isPrime($0) })
-    return (1...n).filter{ isPrime($0) }.count
-}
-
-print(solution(4))
