ce680ee337ea6c823449876a5d40638efe5077d8
Seoyoung Lee <wow9176@naver.com>
2022-03-25 02:05:07 +0900

    [Study] Style cell view

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,12 @@
-import Foundation
-
-
+let str = "z"
+var dict = [Character: Int]()
+for ch in str {
+    if let value = dict[ch] {
+        dict[ch] = value + 1
+    } else {
+        dict[ch] = 0
+    }
+}
+var maxnum = dict.values.max()!
+var maxch = dict.keys.filter({ dict[$0] == maxnum })
+print(maxch.count > 1 ? "?" : maxch[0].uppercased())
