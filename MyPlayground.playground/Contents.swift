10bd8611d5f041b61ef1e7f710babb6c816c1c14
Seoyoung Lee <wow9176@naver.com>
2022-07-30 06:15:42 +0900

    [Study] feat: create new password project

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,189 +1,62 @@
 import Foundation
 
-func solution(_ p:[Int]) -> [Int] {
-    var result = Array(repeating: 0, count: p.count)
-    var arr = p
-
-    if p == p.sorted() { return result }
-
-    for i in 0..<p.count {
-        var min = 10000
-        var minIndex = -1
-        for j in i..<p.count {
-            if arr[j] < min {
-                min = arr[j]
-                minIndex = j
-            }
-        }
-        if arr[i] != arr[minIndex] {
-            let temp = arr[i]
-            arr[i] = arr[minIndex]
-            arr[minIndex] = temp
-            result[i] += 1
-            result[minIndex] += 1
-        }
-    }
-
-    return result
-}
-
-import Foundation
-
-func solution(_ periods:[Int], _ payments:[[Int]], _ estimates:[Int]) -> [Int] {
-    var vip = 0, notVip = 0
-
-    for i in 0..<periods.count {
-        // 가입 기간 2년 미만인 경우
-        if periods[i] < 23 {
-            continue
-        }
-        
-        let currPay = payments[i].reduce(0, +)
-        let estimatedPay = currPay - payments[i][0] + estimates[i]
-        // 2년 이상 ~ 5년 미만
-        if (24...59).contains(periods[i]+1) {
-            if periods[i] == 23 || currPay < 900000 {
-                if estimatedPay >= 900000 {
-                    vip += 1
-                }
-            }
-            if periods[i] > 23 && currPay >= 900000 {
-                if estimatedPay < 900000 {
-                    notVip += 1
-                }
-            }
-        }
-        // 5년 이상
-        if periods[i]+1 >= 60 {
-            if periods[i] == 59 {
-                if currPay < 900000 && estimatedPay >= 600000 {
-                    vip += 1
-                } else if currPay >= 900000 && estimatedPay < 600000 {
-                    notVip += 1
-                }
-            } else {
-                if currPay < 600000 && estimatedPay >= 600000 {
-                    vip += 1
-                } else if currPay >= 600000 && estimatedPay < 600000 {
-                    notVip += 1
-                }
-            }
-        }
-    }
-    return [vip, notVip]
-}
-
-import Foundation
-
-func solution(_ n:Int, _ plans:[String], _ clients:[String]) -> [Int] {
-    var service = Array(repeating: -1, count: n+1) // 부가서비스 첫 등장 위치
-    var datas = [Int]()
-    var result = [Int]()
-
-    // 부가서비스 첫 등장 위치 및 datas 배열 초기화
-    for i in 0..<plans.count {
-        let plan = plans[i].components(separatedBy: " ")
-        datas.append(Int(plan[0])!)
-        // 부가서비스
-        for j in 1..<plan.count {
-            service[Int(plan[j])!] = i
-        }
-    }
-
-    // 각 고객별 요금제 찾기
-    for i in 0..<clients.count {
-        let client = clients[i].components(separatedBy: " ")
-        var start = 0 // 요금제 탐색 최소 시작 위치
-        var end = datas.count-1
-        for j in 1..<client.count {
-            let serviceStart = service[Int(client[j])!]
-            if serviceStart == -1 {
-                result.append(0)
-                start = -1
-                break
-            }
-            start = max(start, service[Int(client[j])!])
-        }
-        if start == -1 { continue }
-        // 필요 데이터보다 더 큰 요금제 찾기
-        let clientData = Int(client[0])!
-        while start <= end {
-            let mid = (start+end) / 2
-            if datas[mid] < clientData {
-                start = mid + 1
-            } else {
-                end = mid - 1
-            }
-        }
-        if start >= datas.count || datas[start] < clientData {
-            result.append(0)
-        } else {
-            result.append(start+1)
-        }
-
-    }
-
-    return result
-}
-
-import Foundation
-
-func solution(_ grid:[String], _ k:Int) -> Int {
-    var map: [[String]] = Array(repeating: Array(repeating: "", count: grid[0].count), count: grid.count)
-    var visit = Array(repeating: Array(repeating: false, count: grid[0].count), count: grid.count)
-    var routes = [String]()
-
-    var result = 100000
-
-    for (i, g) in grid.enumerated() {
-        let str = Array(g)
-        var temp = [String]()
-        for ch in str {
-            temp.append(String(ch))
-        }
-        map[i] = temp
-    }
-
-    // 모든 경로 탐색
-    func dfs(_ r:Int, _ c:Int, _ currString:String) {
-        if r == visit.count-1 && c == visit[0].count-1 {
-            routes.append(currString + map[r][c])
-            return
-        }
-
-        if r >= 0 && r < visit.count && c >= 0 && c < visit[0].count && map[r][c] != "#" {
-            if !visit[r][c] {
-                visit[r][c] = true
-                let str = currString + map[r][c]
-
-                dfs(r-1, c, str)
-                dfs(r+1, c, str)
-                dfs(r, c-1, str)
-                dfs(r, c+1, str)
-
-                visit[r][c] = false
-            }
-        }
-    }
-
-    dfs(0, 0, "")
+func solution(_ s:String) -> Int {
+    if s.count % 2 == 1 { return 0 }
     
-    // 모든 경로 야영횟수 계산
-    for r in routes {
-        let route = Array(r)
-        var i = k
-        var sleepCount = 0
-        while i < route.count-1 {
-            if route[i] != "." {
-                i -= 1
-                if i < 0 { break }
+    var str = Array(s)
+    let dict = ["(": 0, ")": 1, "[": 2, "]": 3, "{": 4, "}": 5]
+    var stk = [Int]()
+    var start = 0, end = s.count-1
+    var rotationCount = 0
+    var x = 0
+    
+    // 올바른 괄호 문자열인지 확인
+    while start <= end {
+        let curr = dict[String(str[start])]!
+        if curr % 2 == 0 { // 여는 괄호인 경우
+            stk.append(curr)
+        } else { // 닫는 괄호인 경우
+            if stk.isEmpty {
+                let lst = dict[String(str[end])]!
+                if curr - lst == 1 { // 짝이 맞는 경우
+                    end -= 1
+                    rotationCount = start+1
+                } else { // 짝이 맞지 않음 - 올바른 괄호 문자열 X
+                    return 0
+                }
             } else {
-                sleepCount += 1
-                i += k
+                if curr - stk.last! == 1 {
+                    stk.removeLast()
+                } else {
+                    return 0
+                }
             }
         }
-        result = min(result, sleepCount)
+        start += 1
     }
-
-    return result
+    
+    print(stk)
+    if !stk.isEmpty { return 0 }
+    
+    // 올바른 문자열 얻기 위한 문자열 회전
+    str = rotationCount > 0 ? Array(str[rotationCount..<str.count] + str[0..<rotationCount]) : str
+    
+    // x 개수 계산
+    stk = []
+    for ch in str {
+        let curr = dict[String(ch)]!
+        if curr % 2 == 0 {
+            if stk.isEmpty {
+                x += 1
+            }
+            stk.append(curr)
+        } else {
+            stk.removeLast()
+        }
+    }
+    
+    
+    return x
 }
+
+print(solution("))())((("))
---
+++ Professional-iOS/Password/Password/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Password
+//
+//  Created by 이서영 on 2022/07/30.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = .white
+        window?.rootViewController = ViewController()
+        
+        return true
+    }
+
+}
+
